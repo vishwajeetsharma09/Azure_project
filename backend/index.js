aad7ebdafb00b43664b8b44ff813dd85336524be
@vishwajeetsharma09 <vishusharmaiit09@gmail.com>
2026-03-28 09:41:26 +0530

    intial

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB connected"));
+
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
